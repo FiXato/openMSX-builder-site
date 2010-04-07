d2cf4e35fa77447a3bcff49367f91d9612e39201
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2010-04-07 10:02:50 +0200

    A bit of a debug version to help detect dupes and roms missing from the openMSX SHA1SUMS:
    * Refactored extraction comments.
    * Added multiple rom output dirs.
    * Added support for locally stored rom archive.
    * Added silently skipping files with known extensions.
    * Added array of all skipped files.

--- public/systemroms_grabber.rb
+++ public/systemroms_grabber.rb
@@ -2,20 +2,40 @@
 require 'open-uri'
 require 'rubygems'
 require 'zip/zip'
+require 'digest/sha1'
+module MyZipfile
+  def extract_with_comments(f, f_path, indentation_level=0)
+    if File.exist?(f_path)
+      puts "#{' ' * indentation_level}Already exists at '#{f_path}'!"
+      return false
+    end
+    FileUtils.mkdir_p(File.dirname(f_path))
+    puts "#{' ' * indentation_level}Extracting to '#{f_path}'"
+    extract(f, f_path)
+  end
+end
+
 class SystemromsGrabber
   BLUEMSX_LOCATION = "http://www.vik.cc/bluemsx/download.html"
-  ROM_DIR = File.expand_path("~/tmp/romdir")
+  ROM_DIR = File.expand_path("~/tmp/romdir_test")
+  SHA1_ROM_DIR = File.expand_path("~/tmp/romdir_test/sha1")
+  MIXED_ROM_DIR = File.expand_path("~/tmp/romdir_test/mixed")
+  SILENTLY_SKIP = %w[mcf bmp xml shortcuts chm dll txt ini exe config]
+  @@skipped_files = []
   def self.grab_zip_from_bluemsx
-    puts "Creating #{ROM_DIR}"
-    FileUtils.mkdir_p(ROM_DIR)
     puts "Reading #{BLUEMSX_LOCATION}"
     doc = open(BLUEMSX_LOCATION).read
     puts "Searching for archive location."
     if (doc =~ /(#{File.join(BLUEMSX_LOCATION.gsub("/download.html",'') + '/.+blueMSXv.+full\.zip')})"/)
-      puts "Archive location found at: #{$1}"
-      puts "Downloading archive. (This may take a while..)"
-      archive = open($1)
-      extract_archive(archive.path)
+      puts "Archive located at: #{$1}"
+      if File.exist?(archive_path = File.expand_path(File.basename($1)))
+        puts "Archive already downloaded as: #{archive_path}"
+        extract_archive(archive_path)
+      else
+        puts "Downloading archive. (This may take a while..)"
+        archive = open($1)
+        extract_archive(archive.path)
+      end
     else
       puts "Could not locate the download on #{BLUEMSX_LOCATION}. Please locate them yourself and download the zip to #{File.dirname(File.expand_path(__FILE__))}"
     end
@@ -24,24 +44,28 @@ class SystemromsGrabber
   def self.extract_archive(filename)
     puts "Extracting #{filename}"
     Zip::ZipFile.open(filename) do |zip_file|
+      puts zip_file.extend MyZipfile
       zip_file.each do |f|
         f_basename = File.basename(f.name)
         f_extension = f_basename.split('.').last
-        if (f_extension == 'rom' && f_extension != f_basename)
-          puts "Found .rom: #{f_basename}"
-          f_path=File.join(ROM_DIR, f_basename)
-          # FileUtils.mkdir_p(File.dirname(f_path))
-          unless File.exist?(f_path)
-            puts "  Extracting '#{f_basename}' to '#{f_path}'"
-            zip_file.extract(f, f_path)
-          else
-            puts "  File '#{f_basename}' ('#{f.name}') already exists in '#{f_path}'!"
-          end
+        if (f_extension.downcase == 'rom' && f_extension != f_basename)
+          puts "Found .rom: #{f_basename} ('#{f.name}')"
+          sha1 = Digest::SHA1.hexdigest(zip_file.read(f))
+          puts "  Sha1: #{sha1}"
+          sha1_path=File.join(SHA1_ROM_DIR,sha1)
+          f_path=File.join(ROM_DIR, f_basename.gsub(/.rom$/,"_#{sha1}.rom"))
+          mixed_path=File.join(MIXED_ROM_DIR, sha1 + '_' + f_basename)
+
+          zip_file.extract_with_comments(f, f_path,3)
+          zip_file.extract_with_comments(f, mixed_path,3)
+          zip_file.extract_with_comments(f, sha1_path,3)
         else
-          puts "Not a .rom, skipping: #{f_basename}"
+          puts "Not a .rom, skipping: #{f_basename}" unless SILENTLY_SKIP.include?(f_extension)
+          @@skipped_files << f_basename
         end
       end
     end
+    # puts "Skipped files: #{@@skipped_files.uniq.join(", ")}"
   end
 end
 puts "Initializing grabber"
